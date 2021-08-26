2349a8dc93bd4f6d3ac3f8ae6ae58c9c16e1ce71
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-08-26 08:58:47 +0300

    added base styles and fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../node_modules/modern-normalize/modern-normalize.css";
+import "../styles/base.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
