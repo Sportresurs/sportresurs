c9711fb4f575f23f24ec0db1f7a64c8c1185b6bb
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-08-27 14:11:02 +0300

    created the markup

---
+++ components/Header/Header.js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <button className={styles.btn_menu}>
+          <Image src="/btnMenu.svg" alt="menu" width={34} height={23}></Image>
+        </button>
+        <Link href="">
+          <a className={styles.nav__logo}>
+            <Image
+              src="/logoHead.svg"
+              alt="logo"
+              width={105}
+              height={15}
+            ></Image>
+          </a>
+        </Link>
+
+        <div className={styles.backdrop}>
+          <div className={styles.box}>
+            <ul className={styles.nav__list}>
+              <li className={styles.nav__item}>
+                <Link href="">
+                  <a className={styles.nav__link}>майданчики</a>
+                </Link>
+              </li>
+              <li className={styles.nav__item}>
+                <Link href="">
+                  <a className={styles.nav__link}>карта</a>
+                </Link>
+              </li>
+              <li className={styles.nav__item}>
+                <Link href="">
+                  <a className={styles.nav__link}>про нас</a>
+                </Link>
+              </li>
+              <li className={styles.nav__item}>
+                <Link href="">
+                  <a className={styles.nav__link}>новини</a>
+                </Link>
+              </li>
+              <li className={styles.nav__item}>
+                <Link href="">
+                  <a className={styles.nav__link}>контакти</a>
+                </Link>
+              </li>
+            </ul>
+
+            <button className={styles.btn_contact}>Зв’яжіться зі мною</button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
