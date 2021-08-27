016fabda40a0d35b1e41422a480a839b9166a230
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-08-27 19:29:39 +0300

    fixes for PR
    add css grid, @svgr,

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,125 +1,100 @@
-import styles from "../../styles/Footer.module.scss";
-import Image from "next/image";
+import s from "./Footer.module.scss";
 import Link from "next/link";
+import Logo from "../../public/svg/logo.svg";
+import LogoMobile from "../../public/svg/logogmobile.svg";
+import FacebookIcon from "../../public/svg/facebook.svg";
+import InstaIcon from "../../public/svg/insta.svg";
+import TwitterIcon from "../../public/svg/twitter.svg";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.container}>
-        <div className={`${styles.wrapper}`}>
-          <div className={styles["logo-desktop"]}>
-            <Image
-              src="/logo.svg"
-              alt="спортресурс"
-              width={280}
-              height={40}
-            ></Image>
+    <footer className={s.footer}>
+      <div className={s.container}>
+        <div className={s.logo}>
+          <div className={s.logoDesktop}>
+            <Logo />
           </div>
 
-          <div className={styles["logo-mobile"]}>
-            <Image
-              src="/logo.svg"
-              alt="спортресурс"
-              width={151}
-              height={22}
-            ></Image>
-          </div>
-
-          <div className={styles["footer--socials"]}>
-            <p className={styles["footer--socials--text"]}>
-              Наші соціальні мережі:
-            </p>
-            <ul className={styles["footer--socials--list"]}>
-              <li className={styles["footer--socials--item"]}>
-                <a
-                  className={styles["footer--socials--item--link"]}
-                  href="https://www.facebook.com/"
-                >
-                  <Image
-                    src="/facebook.svg"
-                    alt="facebook link"
-                    width={31}
-                    height={31}
-                  />
-                </a>
-              </li>
-              <li className={styles["footer--socials--item"]}>
-                <a
-                  className={styles["footer--socials--item--link"]}
-                  href="https://www.instagram.com/"
-                >
-                  <Image
-                    src="/insta.svg"
-                    alt="facebook link"
-                    width={31}
-                    height={31}
-                  />
-                </a>
-              </li>
-              <li className={styles["footer--socials--item"]}>
-                <a
-                  className={styles["footer--socials--item--link"]}
-                  href="https://twitter.com/"
-                >
-                  <Image
-                    src="/twitter.svg"
-                    alt="facebook link"
-                    width={31}
-                    height={31}
-                  />
-                </a>
-              </li>
-            </ul>
+          <div className={s.logoMobile}>
+            <LogoMobile />
           </div>
         </div>
 
-        <div className={styles["flex-wrapper"]}>
-          <div className={styles["footer--courts"]}>
-            <h3 className={styles["footer--courts--title"]}>Майданчики</h3>
-            <div className={styles["footer--courts--wrapper"]}>
-              <ul
-                className={`${styles["footer--courts--list"]} ${styles["footer--courts--list-left"]}`}
+        <div className={s.footerSocials}>
+          <p className={s.footerSocialsText}>Наші соціальні мережі:</p>
+          <ul className={s.footerSocialsList}>
+            <li className={s.footerSocialsItem}>
+              <a
+                className={s.footerSocialsItemLink}
+                href="https://www.facebook.com/"
               >
-                <li className={styles["footer--courts--link"]}>
+                <FacebookIcon />
+              </a>
+            </li>
+            <li className={s.footerSocialsItem}>
+              <a
+                className={s.footerSocialsItemLink}
+                href="https://www.instagram.com/"
+              >
+                <InstaIcon />
+              </a>
+            </li>
+            <li className={s.footerSocialsItem}>
+              <a
+                className={s.footerSocialsItemLink}
+                href="https://twitter.com/"
+              >
+                <TwitterIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={s.flexWrapper}>
+          <div className={s.footerCourts}>
+            <h3 className={s.footerCourtsTitle}>Майданчики</h3>
+            <div className={s.footerCourtsWrapper}>
+              <ul className={`${s.footerCourtsList} ${s.footerCourtsListLeft}`}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Спортивний</a>
                   </Link>
                 </li>
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Дитячий</a>
                   </Link>
                 </li>
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Стріт воркаут</a>
                   </Link>
                 </li>
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Скейт-майданчик</a>
                   </Link>
                 </li>
               </ul>
               <ul
-                className={`${styles["footer--courts--list"]} ${styles["footer--courts--list-right"]}`}
+                className={`${s.footerCourtsList} ${s.footerCourtsListRight}`}
               >
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Баскетбольний</a>
                   </Link>
                 </li>
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Тенісний </a>
                   </Link>
                 </li>
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Футбольний з штучним покриттям </a>
                   </Link>
                 </li>
-                <li className={styles["footer--courts--link"]}>
+                <li className={s.footerCourtsLink}>
                   <Link href={"/"}>
                     <a>Бігові доріжки</a>
                   </Link>
@@ -127,14 +102,20 @@ export default function Footer() {
               </ul>
             </div>
           </div>
-          <div className={styles["footer--contacts"]}>
-            <h3 className={styles["footer--contacts--title"]}>Контакти</h3>
-            <address className={styles["footer--contacts--address"]}>
+          <div className={s.footerContacts}>
+            <h3 className={s.footerContactsTitle}>Контакти</h3>
+            <address className={s.footerContactsAddress}>
               <p>79008, Львів,</p>
               <p>пл. Ринок, 1, каб. 516</p>
-              <a href="tel:+38012345678">+38012345678</a>
+              <a href="tel:+38012345678" className={s.footerTelephone}>
+                +38012345678
+              </a>
             </address>
           </div>
+        </div>
+
+        <div className={s.legal}>
+          <p>@2021. Сайт розроблено KeenEthics . Усі права захищені</p>
         </div>
       </div>
     </footer>
