7cbf1dfbd8bbfc1fd7b93e6e17516c0acc0d8dd6
newsalaar <keyshet@gmail.com>
2021-08-27 10:31:16 +0300

    added svg error

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -1,31 +1,30 @@
 import  styles from './Input.module.scss';
 
-const Input = ({
-  value,
-  onChange,
-  isError = false,
-  label,
-  placeholder
-}) => {
+const Input = ({ value, changeValue, isError = false, label, placeholder }) => {
+  const changeInputValue = (e) => changeValue(e.target.value);
+
   const renderInputField = (clasess = '') => (
-    <input
-      value={value}
-      onChange={onChange}
-      className={`${styles[clasess]} ${styles["custom-input"]} ${isError ? styles['input-error'] : ''}`}
-      placeholder={placeholder}
-    />
+    <>
+      <input
+        value={value}
+        onChange={changeInputValue}
+        className={`${styles[clasess]} ${styles["custom-input"]} ${isError ? styles["input-error"] : ""}`}
+        placeholder={placeholder}
+      />
+      {isError && <i className={styles["error-icon"]} />}
+    </>
   );
 
   return (
-    <>
+    <div className={styles["input-container"]}>
       {label
       ? <label className={styles["input-label"]}>
           {label}
           <br />
-          {renderInputField('margin-top-7')}
+          {renderInputField("margin-top-7")}
         </label>
       : renderInputField()}
-    </>
+    </div>
   );
 };
 
