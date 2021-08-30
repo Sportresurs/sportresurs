541d540f7cd3b0e71d438347bccc7145c8ea9cac
Andrii <andrijkotsupyr@gmail.com>
2021-08-30 22:13:38 +0300

    created convert images function

--- utils/convert.js
+++ utils/convert.js
@@ -17,3 +17,21 @@ const quality = Number.parseInt(IMAGE_QUALITY) || 70;
 const chromaSubsampling = IMAGE_CHROMA_SUBSAMPLING || "4:4:4";
 const fit = IMAGE_FIT || "cover";
 const withoutEnlargement = IMAGE_WITHOUT_ENLARGEMENT === true || true;
+
+function convertImgToBuffer(image) {
+  return sharp(image)
+    .resize(width, height, {
+      fit: fit,
+      withoutEnlargement: withoutEnlargement,
+    })
+    .sharpen()
+    .toFormat(format)
+    .jpeg({
+      quality: quality,
+      chromaSubsampling: chromaSubsampling,
+    })
+    .withMetadata()
+    .toBuffer();
+}
+
+module.exports = convertImgToBuffer;
