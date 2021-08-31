a07c83d67ae7e1ead752651254b60b6263aba445
artemSr <artem.sr997@gmail.com>
2021-08-31 19:15:44 +0300

    add playground details page styles

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
-import Footer from "../components/Footer";
-import Spinner from "../components/Spinner";
-import Rating from "../components/Rating";
+import { useState } from "react";
+import Button from "../components/button";
+import Modal from "../components/modal";
+import PlaygroundModal from "../components/modal/PlaygroundModal";
 
 export default function Home() {
+  const [modal, setModal] = useState(false);
+  const handleOpen = () => {
+    setModal(true);
+  };
+  const handleClose = () => {
+    setModal(false);
+  };
   return (
     <>
-      <Footer />
-      <Spinner />
-      <Rating />
+      <Button variant="green" size="medium" onClick={handleOpen}>
+        Open
+      </Button>
+      <Modal visible={modal} onClose={handleClose}>
+        <PlaygroundModal />
+      </Modal>
     </>
   );
 }
