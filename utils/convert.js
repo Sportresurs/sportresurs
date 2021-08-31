69ee823fe96403e4479d9de5f6ec5c8c354bbd44
Andrii <andrijkotsupyr@gmail.com>
2021-08-31 13:21:43 +0300

    deleted environmental variables

--- utils/convert.js
+++ utils/convert.js
@@ -1,34 +1,24 @@
 const sharp = require("sharp");
 
-const {
-  IMAGE_WIDTH,
-  IMAGE_HEIGHT,
-  IMAGE_FORMAT,
-  IMAGE_QUALITY,
-  IMAGE_CHROMA_SUBSAMPLING,
-  IMAGE_FIT,
-  IMAGE_WITHOUT_ENLARGEMENT,
-} = process.env;
-
-const width = Number.parseInt(IMAGE_WIDTH) || 1200;
-const height = Number.parseInt(IMAGE_HEIGHT) || 1200;
-const format = IMAGE_FORMAT || "jpeg";
-const quality = Number.parseInt(IMAGE_QUALITY) || 70;
-const chromaSubsampling = IMAGE_CHROMA_SUBSAMPLING || "4:4:4";
-const fit = IMAGE_FIT || "cover";
-const withoutEnlargement = IMAGE_WITHOUT_ENLARGEMENT === true || true;
+const WIDTH = 1200;
+const HEIGHT = 1200;
+const FORMAT = "jpeg";
+const QUALITY = 70;
+const CHROMA_SUBSAMPLING = "4:4:4";
+const FIT = "cover";
+const WITHOUT_ENLARGEMENT = true;
 
 function convertImg(image) {
   return sharp(image)
-    .resize(width, height, {
-      fit: fit,
-      withoutEnlargement: withoutEnlargement,
+    .resize(WIDTH, HEIGHT, {
+      fit: FIT,
+      withoutEnlargement: WITHOUT_ENLARGEMENT,
     })
     .sharpen()
-    .toFormat(format)
+    .toFormat(FORMAT)
     .jpeg({
-      quality: quality,
-      chromaSubsampling: chromaSubsampling,
+      quality: QUALITY,
+      chromaSubsampling: CHROMA_SUBSAMPLING,
     })
     .withMetadata()
     .toBuffer();
