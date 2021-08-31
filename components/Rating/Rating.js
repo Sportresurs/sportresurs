bb50a26f9a9eaab479be62053860de6f70db6648
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-08-31 08:19:14 +0300

    fixed rating

--- components/Rating/Rating.js
+++ components/Rating/Rating.js
@@ -1,19 +1,25 @@
 import Rating from "@material-ui/lab/Rating";
+import s from "./Rating.module.scss";
 import { makeStyles } from "@material-ui/core/styles";
 import StarBorderRoundedIcon from "@material-ui/icons/StarBorderRounded";
 import StarRoundedIcon from "@material-ui/icons/StarRounded";
+import { useState } from "react";
 
 // Instruction - for color props use varibles from '../../styles/exportColorVar.module.scss
 // if you want to be able to change value dinamically send "readOnly" property with false,
 // in opposite case send true
 
 export default function Ratings({
-  onChange,
-  defaultValue = 3.5,
+  value = 3.5,
   precision = 0.5,
   readOnly = false,
   color = "#150223",
 }) {
+  const [rating, setRating] = useState(value);
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
   const useStyles = makeStyles(() => ({
     root: {
       color,
@@ -22,25 +28,30 @@ export default function Ratings({
       color,
     },
   }));
-
   const classes = useStyles();
 
   return (
-    <Rating
-      className={classes.root}
-      name="rating"
-      defaultValue={defaultValue}
-      precision={precision}
-      readOnly={readOnly}
-      onChange={onChange}
-      size={"small"}
-      icon={<StarRoundedIcon fontSize="inherit" />}
-      emptyIcon={
-        <StarBorderRoundedIcon
-          fontSize="inherit"
-          className={classes.emptyStar}
-        />
-      }
-    />
+    <div className={s.rating}>
+      <p className={s.value} style={{ color: color }}>
+        {rating}
+      </p>
+      <Rating
+        className={classes.root}
+        name="rating"
+        value={Number(rating)}
+        max={5}
+        precision={precision}
+        readOnly={readOnly}
+        onChange={handleRatingChange}
+        size={"small"}
+        icon={<StarRoundedIcon fontSize="inherit" />}
+        emptyIcon={
+          <StarBorderRoundedIcon
+            fontSize="inherit"
+            className={classes.emptyStar}
+          />
+        }
+      />
+    </div>
   );
 }
