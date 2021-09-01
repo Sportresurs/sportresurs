34d78ef0efb1f9f9fdf3af9f1da52458af811afb
Vlad Remeniuk <68018924+Dr-Frame@users.noreply.github.com>
2021-09-01 09:23:19 +0300

    added about us section (#23)
    * added about section
    
    * changed the video link
    
    * fixed PR
    
    * add container placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import Footer from "../components/Footer";
-import Spinner from "../components/Spinner";
-import Rating from "../components/Rating";
+import About from "../components/About";
 
 export default function Home() {
   return (
     <>
-      <Footer />
-      <Spinner />
-      <Rating />
+      <About />
     </>
   );
 }
