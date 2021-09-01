3cb4f854bb6bff73c45566d83908ce76a6fd85dc
artemSr <75668627+artemSr@users.noreply.github.com>
2021-09-01 09:37:48 +0300

    add page layout (#20)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
+import Layout from "../components/layout";
 import "../styles/base.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp;
