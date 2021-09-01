fcc6db86832d8bb56424ff6a1d17149372925ab5
Bohdan-R <68867425+Bohdan-R@users.noreply.github.com>
2021-09-01 10:29:53 +0300

    Dev 35 implement header (#12)
    * created the markup
    
    * created desctop styles
    
    * added mobile styles
    
    * added open close function menu
    
    * some fixes + i svgr/webpack
    
    * fix
    
    * made responsive
    
    * fix
    
    * fix className.bind
    
    * resolve conflict
    
    * resolve conflict 1.1
    
    * r c
    
    * r c 1.1
    
    * r c 1.2
    
    Co-authored-by: dimirolz <dimirolz@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Footer from "../components/Footer";
+import Spinner from "../components/Spinner";
+import Rating from "../components/Rating";
 import About from "../components/About";
 
 export default function Home() {
   return (
     <>
+      <Footer />
+      <Spinner />
+      <Rating />
       <About />
     </>
   );
