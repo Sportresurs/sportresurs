6db7387a0e567bbcf8e4e6666b5fc5dfa9b35fdb
dimirolz <dimirolz@gmail.com>
2021-09-02 12:15:43 +0300

    move SENTRY_DSN to .env

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,8 +3,6 @@ import * as Sentry from "@sentry/nextjs";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    "https://211ad6a7e8e34c839b2ec2e66e6fc2d2@o985913.ingest.sentry.io/5942355",
+  dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
 });
