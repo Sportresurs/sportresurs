b150ef640204ce244f763e00d679513d23475782
artemSr <75668627+artemSr@users.noreply.github.com>
2021-09-03 19:57:37 +0300

    add playground details page styles (#24)
    * add playground details page styles
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix layout page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page
    
    * fix playground details page

--- next.config.js
+++ next.config.js
@@ -5,9 +5,19 @@ const moduleExports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+      ],
     });
-
     return config;
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
-import Footer from "../components/Footer";
-import Spinner from "../components/Spinner";
-import Rating from "../components/Rating";
+import { useState } from "react";
+import image from "../components/PlaygroundModalContent/images/image.png";
+import PlaygroundModal from "../components/PlaygroundModal";
+import Button from "../components/Button";
 import About from "../components/About";
 
 export default function Home() {
+  const [modal, setModal] = useState(false);
+  const handleOpen = () => {
+    setModal(true);
+  };
+  const handleClose = () => {
+    setModal(false);
+  };
+  const playground = {
+    id: 1,
+    address: "Тернопільська, 13а, Львів",
+    districtColor: "yellow",
+    district: "Сихівський",
+    type: "спортивний",
+    purpose: "не зазначено",
+    area: "1630 м. кв.",
+    covering: "штучна трава",
+    access: "безкоштовний",
+    opening: "08:00 - 22:00",
+    lighting: "є",
+    additionally:
+      "огорожа, ворота, тенісний стіл, вуличні тренажери,смітники, лавки, комерційні години (бронювання за телефоном)",
+    img: image,
+  };
   return (
     <>
-      <Footer />
-      <Spinner />
-      <Rating />
+      <Button variant="green" size="large" onClick={handleOpen}>
+        Open
+      </Button>
+      <PlaygroundModal
+        visible={modal}
+        onClose={handleClose}
+        playground={playground}
+      />
       <About />
     </>
   );
