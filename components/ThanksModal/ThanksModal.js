6cd259f6df8171fd639214001254f659b98baf87
Vlad Remeniuk <68018924+Dr-Frame@users.noreply.github.com>
2021-09-08 16:53:28 +0300

    ticket #17 add thanks modal component (#42)
    * add thanks modal component
    
    * Update ThanksModal.module.scss

---
+++ components/ThanksModal/ThanksModal.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import Button from "../Button";
+import s from "./ThanksModal.module.scss";
+import Modal from "../Modal";
+import ThumbUpIcon from "../../public/svg/thumbUp.svg";
+
+export default function ThanksModal({ isShow, variant = "client", onClose }) {
+  return (
+    <Modal visible={isShow} onClose={onClose}>
+      <div className={s.modal}>
+        <ThumbUpIcon className={s.icon} />
+        <h3 className={s.title}>Готово!</h3>
+        <p className={s.text}>
+          {variant === "client"
+            ? "Ми отримали ваші дані і скоро вам зателефонуємо"
+            : "Ваші зміни збережено"}
+        </p>
+        <Button variant="black" size="medium" onClick={onClose}>
+          Добре
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+ThanksModal.propTypes = {
+  isShow: PropTypes.bool.isRequired,
+  variant: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+};
