866054d10d1f1143a6260698fb5d5eb8676d945e
Sergey Cherkasov <33036302+xaarxus@users.noreply.github.com>
2021-09-08 16:26:00 +0300

    hotfix/#75-change-Input-props (#44)
    * change-Input-props & rename dir
    
    * fix prettier error
    
    * Update Input.jsx

---
+++ components/Input/Input.jsx
@@ -0,0 +1,35 @@
+import classNames from "classnames";
+import styles from "./Input.module.scss";
+import Error from "../../public/svg/input_error.svg";
+
+const Input = ({ value, isError = false, label, placeholder, ...rest }) => {
+  const combinedClassName = classNames(
+    styles.customInput,
+    isError ? styles.inputError : ""
+  );
+
+  const renderInputField = () => (
+    <>
+      <input
+        value={value}
+        className={combinedClassName}
+        placeholder={placeholder}
+        {...rest}
+      />
+      {isError && (
+        <div className={styles.errorIcon}>
+          <Error />
+        </div>
+      )}
+    </>
+  );
+
+  return (
+    <div className={styles.inputContainer}>
+      {label && <label className={styles.inputLabel}>{label}</label>}
+      {renderInputField()}
+    </div>
+  );
+};
+
+export default Input;
