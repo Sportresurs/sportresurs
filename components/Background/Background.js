f17bb8449d22d1ae024aec31c27da02b19c8cca8
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-09-09 08:58:05 +0300

    fixes

--- components/Background/Background.js
+++ components/Background/Background.js
@@ -1,45 +1,25 @@
 import PropTypes from "prop-types";
 import s from "./Background.module.scss";
-import BasketBg from "../../public/svg/basketBg.svg";
-import FootballBg from "../../public/svg/footballBg.svg";
-import FootballBgLilac from "../../public/svg/footballBgLilac.svg";
-import TenisBg from "../../public/svg/tenisBg.svg";
-import TenisBgGreen from "../../public/svg/tenisBgGreen.svg";
-import colors from "../../styles/exportColorVars.module.scss";
 
 const Background = ({ variant }) => (
   <>
     {variant === "homePage" && (
       <>
-        <div className={s.homeTopLeftBasketBg}>
-          <BasketBg fill={colors.colorAccentGreen} />
-        </div>
-        <div className={s.homeTopRightBasketBg}>
-          <BasketBg fill={colors.colorAccentBlue} />
-        </div>
-        <div className={s.homeBottomLeftBasketBg}>
-          <TenisBg />
-        </div>
-        <div className={s.homeBottomRightBasketBg}>
-          <FootballBg />
-        </div>
+        <div className={s.homeTopLeftBasketBg}></div>
+        <div className={s.homeTopRightBasketBg}></div>
+        <div className={s.homeBottomLeftTenisBg}></div>
+        <div className={s.homeBottomRightBasketBg}></div>
       </>
     )}
     {variant === "playgroundsPage" && (
       <>
-        <div className={s.playgroundsLeftBg}>
-          <FootballBgLilac />
-        </div>
-        <div className={s.playgroundsRightBg}>
-          <BasketBg />
-        </div>
+        <div className={s.playgroundsLeftBg}></div>
+        <div className={s.playgroundsRightBg}></div>
       </>
     )}
     {variant === "mapList" && (
       <>
-        <div className={s.mapTenisBg}>
-          <TenisBgGreen />
-        </div>
+        <div className={s.mapTenisBg}></div>
       </>
     )}
   </>
--- pages/playgrounds.js
+++ pages/playgrounds.js
@@ -8,19 +8,17 @@ export default function Playgrounds() {
   return (
     <>
       <section className={s.courts}>
-        <div className={s.wrapper}>
-          <Background variant="playgroundsPage" />
-          <Grid>
-            <h1 className={s.title}>Майданчики</h1>
-            <ul className={s.list}>
-              {data.playgroundsList.map((court) => (
-                <li key={court.id} className={s.listItem}>
-                  <CourtCard courtInfo={court} variant="courtList" />
-                </li>
-              ))}
-            </ul>
-          </Grid>
-        </div>
+        <Background variant="playgroundsPage" />
+        <Grid>
+          <h1 className={s.title}>Майданчики</h1>
+          <ul className={s.list}>
+            {data.playgroundsList.map((court) => (
+              <li key={court.id} className={s.listItem}>
+                <CourtCard courtInfo={court} variant="courtList" />
+              </li>
+            ))}
+          </ul>
+        </Grid>
       </section>
     </>
   );
