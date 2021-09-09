d9e42fae907bc2bd3c78509824b6ddb6a71c3c78
Sergey Cherkasov <keyshet@gmail.com>
2021-09-09 15:33:25 +0300

    update emailSerder

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -1,11 +1,7 @@
 import nodemailer from "nodemailer";
 import * as Sentry from "@sentry/nextjs";
 
-const EMAIL_LOGIN = "sportresurs12345@gmail.com";
-const EMAIL_PASS = "qw12er34qw12";
-
-const RECIPIENT_EMAIL = "keyshet@gmail.com";
-const EMAIL_TITLE = "Contact Us";
+const { EMAIL_LOGIN, EMAIL_PASS, RECIPIENT_EMAIL } = process.env;
 
 const transporter = nodemailer.createTransport({
   service: "Gmail",
@@ -16,20 +12,30 @@ const transporter = nodemailer.createTransport({
 });
 
 const emailSender = (name, phoneNumber, details) => {
+  const EMAIL_TITLE = `${name}: звернення з сайту майданчиків через форму зворотнього зв'язку`;
+  const message = `
+    <p>Шановний адмін сайту Спортресурс,<br>
+    Користувач залишив запит для зворотного зв'язку:</p>
+    <p>Ім'я: ${name}</p>
+    <p>Номер телефону: ${phoneNumber}</p>
+    <p>Деталі: ${details}</p>
+    <p>Будь ласка, якщо візьмете запит в роботу, повідомте інших адміністраторів.<br>
+    Дякуємо!</p>
+  `;
+
   try {
     transporter.sendMail({
       to: RECIPIENT_EMAIL,
       subject: EMAIL_TITLE,
-      html: `
-        <p>Имя: ${name}</p>
-        <p>Номер телефона: ${phoneNumber}</p>
-        <p>Детали сообщения: ${details}</p>
-      `,
+      html: message,
     });
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error(err);
+    console.error(err, message);
     Sentry.captureException(err);
+    Sentry.captureMessage(
+      `Повідомлення з форми зворотнього зв'язку яке не вдалося надіслаты\n${message}`
+    );
   }
 };
 
