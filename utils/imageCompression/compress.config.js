ea0704b36cd4c22f0a3f1fcd6797e89d5220e56b
Andrii <andrijkotsupyr@gmail.com>
2021-09-10 11:16:55 +0300

    updated compress config

--- utils/imageCompression/compress.config.js
+++ utils/imageCompression/compress.config.js
@@ -4,30 +4,37 @@ const config = convict({
   imageWidth: {
     format: "int",
     default: 1200,
+    env: "IMAGE_WIDTH"
   },
   imageHeight: {
     format: "int",
     default: 1200,
+    env: "IMAGE_HEIGHT"
   },
   imageFormat: {
     format: String,
     default: "jpeg",
+    env: "IMAGE_FORMAT"
   },
   imageQuality: {
     format: "int",
     default: 70,
+    env: "IMAGE_QUALITY"
   },
   imageChromaSubsampling: {
     format: String,
     default: "4:4:4",
+    env: "IMAGE_CHROMA_SUBSAMPLING"
   },
   imageFit: {
     format: String,
     default: "cover",
+    env: "IMAGE_FIT"
   },
   imageWithoutEnlargement: {
     format: "Boolean",
     default: true,
+    env: "IMAGE_WITHOUT_ENLARGEMENT"
   },
 });
 
