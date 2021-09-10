e83a45a98b2dca2c66ca9c27980c86232251ec1d
Sergey Cherkasov <keyshet@gmail.com>
2021-09-10 06:26:07 +0300

    added comment

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -1,6 +1,13 @@
 import nodemailer from "nodemailer";
 import * as Sentry from "@sentry/nextjs";
 
+// This approach requires a valid email address (Gmail).
+// On your email account, you will need to give access to login
+// from third-party applications https://myaccount.google.com/lesssecureapps?
+// Also, this approach has limitations (500 emails per day / at a time).
+// If the mail got into the "SPAM" section, you need to indicate the sender as "verified"
+// After you indicate that this is a verified sender, email will stop getting into "SPAM"
+
 const { EMAIL_LOGIN, EMAIL_PASS, RECIPIENT_EMAIL } = process.env;
 
 const transporter = nodemailer.createTransport({
@@ -34,7 +41,7 @@ const emailSender = (name, phoneNumber, details) => {
     console.error(err, message);
     Sentry.captureException(err);
     Sentry.captureMessage(
-      `Повідомлення з форми зворотнього зв'язку яке не вдалося надіслаты\n${message}`
+      `Повідомлення з форми зворотнього зв'язку яке не вдалося надіслати\n${message}`
     );
   }
 };
