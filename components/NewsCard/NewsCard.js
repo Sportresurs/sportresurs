fc1de50f72232959b9ddd4115e9642a3dee98f13
Andrii <andrijkotsupyr@gmail.com>
2021-09-12 22:31:28 +0300

    deleted color picker function

--- components/NewsCard/NewsCard.js
+++ components/NewsCard/NewsCard.js
@@ -1,27 +1,7 @@
 import Image from "next/image";
 import styles from "./NewsCard.module.scss";
-import {
-  colorAccentPurple,
-  colorAccentBlue,
-  colorAccentGreen,
-  colorAccentOrange,
-  colorAccentYellow,
-  colorAccentRed,
-} from "../../styles/exportColorVars.module.scss";
 
 export default function NewsCard({ data }) {
-  function pickColor() {
-    const colorsArr = [
-      colorAccentPurple,
-      colorAccentBlue,
-      colorAccentGreen,
-      colorAccentOrange,
-      colorAccentYellow,
-      colorAccentRed,
-    ];
-    const randomColor = colorsArr[Math.floor(Math.random() * colorsArr.length)];
-    return randomColor;
-  }
 
   return (
     <>
@@ -32,7 +12,7 @@ export default function NewsCard({ data }) {
           layout="fill"
           className={styles.image}
         />
-        <div className={styles.info} style={{ background: pickColor() }}>
+        <div className={styles.info} >
           {data.text}
         </div>
       </div>
