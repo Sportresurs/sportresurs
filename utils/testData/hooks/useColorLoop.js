00749d837283c3dc086d84c596996c8e5d4dcc93
Andrii <andrijkotsupyr@gmail.com>
2021-09-12 22:24:25 +0300

    created useColorLoop hook

---
+++ utils/testData/hooks/useColorLoop.js
@@ -0,0 +1,27 @@
+import { useCallback, useMemo } from "react";
+import { SequentialRoundRobin } from "round-robin-js";
+import {
+  colorAccentPurple,
+  colorAccentBlue,
+  colorAccentGreen,
+  colorAccentOrange,
+  colorAccentYellow,
+  colorAccentRed,
+} from "../../../styles/exportColorVars.module.scss";
+
+export default function useColorLoop() {
+  const colors = useMemo(
+    () =>
+      new SequentialRoundRobin([
+        colorAccentPurple,
+        colorAccentBlue,
+        colorAccentGreen,
+        colorAccentOrange,
+        colorAccentYellow,
+        colorAccentRed,
+      ]),
+    []
+  );
+
+  return useCallback(() => colors.next().value, []);
+}
