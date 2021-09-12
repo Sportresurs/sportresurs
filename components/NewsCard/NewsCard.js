11ebc5ed9b5b0cf3547dbcbbe5afa88ba07eeb50
Andrii <andrijkotsupyr@gmail.com>
2021-09-12 22:35:21 +0300

    updated NewsCard component

--- components/NewsCard/NewsCard.js
+++ components/NewsCard/NewsCard.js
@@ -1,21 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./NewsCard.module.scss";
 
-export default function NewsCard({ data }) {
-
+export default function NewsCard({ newsData, color }) {
   return (
-    <>
+    <Link href={newsData.url}>
       <div className={styles.card}>
         <Image
-          src={data.img}
+          src={newsData.imgUrl}
           alt="news-card"
           layout="fill"
           className={styles.image}
         />
-        <div className={styles.info} >
-          {data.text}
+        <div className={styles.info} style={{background: color}}>
+          {newsData.text}
         </div>
       </div>
-    </>
+    </Link>
   );
 }
