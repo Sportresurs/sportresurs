21ac434576db54f236d1839794103cafaa1ede3d
Andrii <andrijkotsupyr@gmail.com>
2021-09-16 10:01:26 +0300

    T-67 change model name

--- models/purpose-area.js
+++ models/purpose-area.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "PurposeArea",
+      modelName: "purpose-area",
       underscored: true,
     }
   );
