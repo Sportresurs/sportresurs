9e774d1ff72def796b8173e00eb6e0178667edd5
Andrii <andrijkotsupyr@gmail.com>
2021-09-16 14:04:40 +0300

    T-67 add associations

--- models/purpose.js
+++ models/purpose.js
@@ -2,14 +2,12 @@ const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Purpose extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    // eslint-disable-next-line no-unused-vars
     static associate(models) {
-      // define association here
+      this.belongsToMany(models.Area, {
+        through: "purpose-area",
+        foreignKey: "purpose_id",
+        otherKey: "area_id",
+      });
     }
   }
   Purpose.init(
