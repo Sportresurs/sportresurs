fdbb162702b983f93ef369a628c727ddb6797a90
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-09-17 10:33:17 +0300

    T-2 Add autocomplete

--- components/SearchSection/SearchSection.js
+++ components/SearchSection/SearchSection.js
@@ -1,4 +1,11 @@
+import Link from "next/link";
 import { useState } from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import scriptLoader from "react-async-script-loader";
+import classNames from "classnames/bind";
 import Button from "../Button/Button";
 import { Grid } from "../Grid/Grid";
 import styles from "./SearchSection.module.scss";
@@ -9,77 +16,155 @@ import Child from "../../public/svg/childrenIcon.svg";
 import Tenis from "../../public/svg/tenisIcon.svg";
 import Close from "../../public/svg/closeAutoCIcon.svg";
 
-export default function SearchSection() {
-  const [value, setValue] = useState("");
+const cx = classNames.bind(styles);
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
+function SearchSection({ isScriptLoaded, isScriptLoadSucceed }) {
+  const [address, setAddress] = useState("");
+  const [coordinates, setCoordinates] = useState(null);
 
   const handleInputClear = () => {
-    setValue("");
+    setAddress("");
   };
 
-  return (
-    <section className={styles.section}>
-      <Grid>
-        <div className={styles.box}>
-          <h2 className={styles.title}>Знайди свій ідеальний майданчик</h2>
+  const handleChange = (value) => {
+    setAddress(value);
+  };
 
-          <form className={styles.form}>
-            <div className={styles.inputBox}>
-              <input
-                className={styles.input}
-                placeholder="Введіть назву вулиці"
-                value={value}
-                onChange={handleChange}
-              />
+  const handleSelect = async (value) => {
+    const result = await geocodeByAddress(value);
+    const ll = await getLatLng(result[0]);
+    setAddress(value);
+    setCoordinates(ll);
+  };
 
-              {value && (
-                <button
-                  className={styles.fromBtnClose}
-                  type="button"
-                  onClick={handleInputClear}
+  /* const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Search cor submit: ", coordinates);
+    handleInputClear();
+  }; */
+
+  const handleSubmit = () => {
+    handleInputClear();
+  };
+
+  const searchOptions = () => {
+    if (typeof window !== "undefined") {
+      return {
+        location: new window.google.maps.LatLng(49.842957, 24.031111),
+        radius: 30000,
+        types: ["address"],
+      };
+    }
+    return null;
+  };
+
+  const onError = (status, clearSuggestions) => {
+    clearSuggestions();
+  };
+
+  if (isScriptLoaded && isScriptLoadSucceed) {
+    return (
+      <section className={styles.section}>
+        <Grid>
+          <div className={styles.box}>
+            <h2 className={styles.title}>Знайди свій ідеальний майданчик</h2>
+
+            <form className={styles.form} /* onSubmit={handleSubmit} */>
+              <div className={styles.inputBox}>
+                <PlacesAutocomplete
+                  value={address}
+                  onChange={handleChange}
+                  onSelect={handleSelect}
+                  searchOptions={searchOptions()}
+                  onError={onError}
                 >
-                  <Close className={styles.fromBtnCloseIcon}></Close>
-                </button>
-              )}
-            </div>
+                  {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+                    <div>
+                      <input
+                        {...getInputProps({
+                          placeholder: "Введіть назву вулиці",
+                          className: styles.input,
+                        })}
+                      />
+                      {suggestions.length >= 1 && (
+                        <div className={cx("autocomplete")}>
+                          {suggestions.map((suggestion) => (
+                            <div
+                              {...getSuggestionItemProps(suggestion, {
+                                className: cx("autocompleteItem", {
+                                  hover: suggestion.active,
+                                }),
+                              })}
+                              key={suggestion.description}
+                            >
+                              <span className={cx("autocompleteMainText")}>
+                                {suggestion.formattedSuggestion.mainText}
+                              </span>
+                              <span className={cx("autocompleteSecondaryText")}>
+                                {suggestion.formattedSuggestion.secondaryText}
+                              </span>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </PlacesAutocomplete>
 
-            <button className={styles.formBtn} type="button">
-              <span className={styles.formBtnContent}>Пошук майданчика</span>
-              <SearchIcon className={styles.formBtnIcon} />
-            </button>
-          </form>
+                {address && (
+                  <button
+                    className={styles.fromBtnClose}
+                    type="button"
+                    onClick={handleInputClear}
+                  >
+                    <Close className={styles.fromBtnCloseIcon}></Close>
+                  </button>
+                )}
+              </div>
+              <Link href="/playgrounds">
+                <a className={styles.formBtn} onClick={handleSubmit}>
+                  <span className={styles.formBtnContent}>
+                    Пошук майданчика
+                  </span>
+                  <SearchIcon className={styles.formBtnIcon} />
+                </a>
+              </Link>
+            </form>
 
-          <ul className={styles.btnList}>
-            <li className={styles.btnItem}>
-              <Button variant="orange" size="medium">
-                <span className={styles.btnContent}>Баскетбольний</span>
-                <Basket className={styles.btnIcon} />
-              </Button>
-            </li>
-            <li className={styles.btnItem}>
-              <Button variant="green" size="medium">
-                <span className={styles.btnContent}>Футбольний</span>
-                <Football className={styles.btnIcon} />
-              </Button>
-            </li>
-            <li className={styles.btnItem}>
-              <Button variant="blue" size="medium">
-                <span className={styles.btnContent}>Дитячий</span>
-                <Child className={styles.btnIcon} />
-              </Button>
-            </li>
-            <li className={styles.btnItem}>
-              <Button variant="lilac" size="medium">
-                <span className={styles.btnContent}>Тенісний</span>
-                <Tenis className={styles.btnIcon} />
-              </Button>
-            </li>
-          </ul>
-        </div>
-      </Grid>
-    </section>
-  );
+            <ul className={styles.btnList}>
+              <li className={styles.btnItem}>
+                <Button variant="orange" size="medium">
+                  <span className={styles.btnContent}>Баскетбольний</span>
+                  <Basket className={styles.btnIcon} />
+                </Button>
+              </li>
+              <li className={styles.btnItem}>
+                <Button variant="green" size="medium">
+                  <span className={styles.btnContent}>Футбольний</span>
+                  <Football className={styles.btnIcon} />
+                </Button>
+              </li>
+              <li className={styles.btnItem}>
+                <Button variant="blue" size="medium">
+                  <span className={styles.btnContent}>Дитячий</span>
+                  <Child className={styles.btnIcon} />
+                </Button>
+              </li>
+              <li className={styles.btnItem}>
+                <Button variant="lilac" size="medium">
+                  <span className={styles.btnContent}>Тенісний</span>
+                  <Tenis className={styles.btnIcon} />
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </Grid>
+      </section>
+    );
+  }
+  return <div>{coordinates && <p>{coordinates}</p>}</div>;
 }
+
+export default scriptLoader([
+  "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCQpQm5NpH4H9sGBd66F8UzhPuAsyFEZTA",
+])(SearchSection);
