861a6f739cc44085988a06717c7b061abec49f30
Vlad Remeniuk <68018924+Dr-Frame@users.noreply.github.com>
2021-09-22 12:34:56 +0300

    T 22 implement login page for admin (#78)
    * t-22-add login page
    
    * t-22-update styles
    
    * t-22-change pages folder structure
    
    * t-22-update pages folder structure
    
    * Update Login.module.scss
    
    * Update index.js

---
+++ pages/login/Login.js
@@ -0,0 +1,18 @@
+import styles from "./Login.module.scss";
+import Button from "../../components/Button";
+
+export default function Login() {
+  return (
+    <section className={styles.login}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <h1 className={styles.title}>Вітаємо в СпортРесурс</h1>
+          <p className={styles.text}>Активуйте свій доступ адміна</p>
+          <Button variant="green" className={styles.button}>
+            Google Login
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
