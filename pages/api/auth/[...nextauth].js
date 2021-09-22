f013168c2283dc1d1ac4b56cd268a5981633ca9b
Sergey Cherkasov <keyshet@gmail.com>
2021-09-22 12:09:52 +0300

    T-115 add auth only admins

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { withSentry } from "@sentry/nextjs";
+import { User } from "../../../models";
 
 const options = {
   providers: [
@@ -9,7 +10,16 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  algorithms: ["HS256"],
+  callbacks: {
+    async signIn(user) {
+      const users = await User.findAll();
+      const emails = users.map((el) => el.email);
+      if (emails.includes(user.email)) {
+        return true;
+      }
+      return false;
+    },
+  },
 };
 
 function Login(req, res) {
