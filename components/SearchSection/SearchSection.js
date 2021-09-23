9739f628ea1616ce335bb66744f6a06c82ff566c
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-09-23 14:52:19 +0300

    T-2 Change script load

--- components/SearchSection/SearchSection.js
+++ components/SearchSection/SearchSection.js
@@ -1,10 +1,10 @@
+import Script from "next/script";
 import Link from "next/link";
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from "react-places-autocomplete";
-import scriptLoader from "react-async-script-loader";
 import classNames from "classnames/bind";
 import { LVIV_COORDINATES } from "../../utils/constants";
 import { Context } from "../../context";
@@ -16,10 +16,13 @@ import Close from "../../public/svg/closeAutoCIcon.svg";
 const cx = classNames.bind(styles);
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-function SearchSection({ isScriptLoaded, isScriptLoadSucceed }) {
+function SearchSection() {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [address, setAddress] = useState("");
   const [coordinates, setCoordinates] = useState(null);
   const { handleCoordinates } = useContext(Context);
+
+  useEffect(() => window.google && setIsLoaded(true), []);
 
   const handleInputClear = () => {
     setAddress("");
@@ -31,21 +34,10 @@ function SearchSection({ isScriptLoaded, isScriptLoadSucceed }) {
 
   const handleSelect = async (value) => {
     const result = await geocodeByAddress(value);
-    const LatLng = await getLatLng(result[0]);
+    const latLng = await getLatLng(result[0]);
     setAddress(value);
-    setCoordinates(LatLng);
+    setCoordinates(latLng);
   };
-
-  /* const searchOptions = () => {
-    window.google && {
-      location: new window.google.maps.LatLng(
-        LVIV_COORDINATES.lat,
-        LVIV_COORDINATES.lng
-      ),
-      radius: 30000,
-      types: ["address"],
-    };
-  }; */
 
   const searchOptions = () =>
     window.google && {
@@ -57,77 +49,99 @@ function SearchSection({ isScriptLoaded, isScriptLoadSucceed }) {
       types: ["address"],
     };
 
-  const onError = (status, clearSuggestions) => {
+  const onError = (_, clearSuggestions) => {
     clearSuggestions();
   };
 
-  if (isScriptLoaded && isScriptLoadSucceed) {
-    return (
+  return (
+    <>
+      <Script
+        type="text/javascript"
+        src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
+        onLoad={() => {
+          setIsLoaded(true);
+        }}
+      />
       <section className={styles.section}>
         <div className={styles.box}>
           <h2 className={styles.title}>Знайди свій ідеальний майданчик</h2>
 
           <form className={styles.form}>
-            <div className={styles.inputBox}>
-              <PlacesAutocomplete
-                value={address}
-                onChange={handleChange}
-                onSelect={handleSelect}
-                searchOptions={searchOptions()}
-                onError={onError}
-              >
-                {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-                  <div>
-                    <input
-                      {...getInputProps({
-                        placeholder: "Введіть назву вулиці",
-                        className: styles.input,
-                      })}
-                    />
-                    {suggestions.length >= 1 && (
-                      <div className={cx("autocomplete")}>
-                        {suggestions.map((suggestion) => (
-                          <div
-                            {...getSuggestionItemProps(suggestion, {
-                              className: cx("autocompleteItem", {
-                                hover: suggestion.active,
-                              }),
-                            })}
-                            key={suggestion.description}
-                          >
-                            <span className={cx("autocompleteMainText")}>
-                              {suggestion.formattedSuggestion.mainText}
-                            </span>
-                            <span className={cx("autocompleteSecondaryText")}>
-                              {suggestion.formattedSuggestion.secondaryText}
-                            </span>
+            {!isLoaded && <div className={styles.boxPlug}></div>}
+            {isLoaded && (
+              <>
+                <div className={styles.inputBox}>
+                  <PlacesAutocomplete
+                    value={address}
+                    onChange={handleChange}
+                    onSelect={handleSelect}
+                    searchOptions={searchOptions()}
+                    onError={onError}
+                  >
+                    {({
+                      getInputProps,
+                      suggestions,
+                      getSuggestionItemProps,
+                    }) => (
+                      <div>
+                        <input
+                          {...getInputProps({
+                            placeholder: "Введіть назву вулиці",
+                            className: styles.input,
+                          })}
+                        />
+                        {suggestions.length >= 1 && (
+                          <div className={cx("autocomplete")}>
+                            {suggestions.map((suggestion) => (
+                              <div
+                                {...getSuggestionItemProps(suggestion, {
+                                  className: cx("autocompleteItem", {
+                                    hover: suggestion.active,
+                                  }),
+                                })}
+                                key={suggestion.description}
+                              >
+                                <span className={cx("autocompleteMainText")}>
+                                  {suggestion.formattedSuggestion.mainText}
+                                </span>
+                                <span
+                                  className={cx("autocompleteSecondaryText")}
+                                >
+                                  {suggestion.formattedSuggestion.secondaryText}
+                                </span>
+                              </div>
+                            ))}
                           </div>
-                        ))}
+                        )}
                       </div>
                     )}
-                  </div>
-                )}
-              </PlacesAutocomplete>
+                  </PlacesAutocomplete>
 
-              {address && (
-                <button
-                  className={styles.fromBtnClose}
-                  type="button"
-                  onClick={handleInputClear}
-                >
-                  <Close className={styles.fromBtnCloseIcon}></Close>
-                </button>
-              )}
-            </div>
-            <Link href="/playgrounds">
-              <a
-                className={styles.formBtn}
-                onClick={handleCoordinates(coordinates)}
-              >
-                <span className={styles.formBtnContent}>Пошук майданчика</span>
-                <SearchIcon className={styles.formBtnIcon} />
-              </a>
-            </Link>
+                  {address && (
+                    <button
+                      className={styles.fromBtnClose}
+                      type="button"
+                      onClick={handleInputClear}
+                    >
+                      <Close className={styles.fromBtnCloseIcon}></Close>
+                    </button>
+                  )}
+                </div>
+                <Link href="/playgrounds">
+                  <a
+                    className={cx("formBtn", {
+                      disabled: !address,
+                    })}
+                    onClick={handleCoordinates(coordinates)}
+                  >
+                    <span className={styles.formBtnContent}>
+                      Пошук майданчика
+                    </span>
+                    <SearchIcon className={styles.formBtnIcon} />
+                  </a>
+                </Link>
+              </>
+            )}
           </form>
 
           <ul className={styles.btnList}>
@@ -154,11 +168,8 @@ function SearchSection({ isScriptLoaded, isScriptLoadSucceed }) {
           </ul>
         </div>
       </section>
-    );
-  }
-  return <div></div>;
+    </>
+  );
 }
 
-export default scriptLoader([
-  `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`,
-])(SearchSection);
+export default SearchSection;
