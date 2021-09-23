8e0e42d74958bbcc7659b5e8bb95da7160aea4f4
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-09-23 14:57:11 +0300

    T-2 Remove unneeded markup and npm library

--- pages/playgrounds.js
+++ pages/playgrounds.js
@@ -1,22 +1,13 @@
-import { useContext } from "react";
-import { Context } from "../context";
 import s from "../styles/Playgrounds.module.scss";
 import CourtCard from "../components/CourtCard";
 import { Grid } from "../components/Grid";
 import data from "../utils/testData/testArrs";
 
 export default function Playgrounds() {
-  const { coordinates } = useContext(Context);
-
   return (
     <div className={s.background}>
       <section className={s.courts}>
         <Grid>
-          {coordinates && (
-            <div>
-              <p>Lat{coordinates.lat}</p>
-            </div>
-          )}
           <h1 className={s.title}>Майданчики</h1>
           <ul className={s.list}>
             {data.playgroundsList.map((court) => (
