764903f1c084aeda45792b1723d886bdf0b25124
Vlad Remeniuk <68018924+Dr-Frame@users.noreply.github.com>
2021-09-28 16:57:42 +0300

    T-148 Request page changes (#98)
    * t-148-request-page-changes
    
    * t-148-fix

--- pages/requests/Requests.js
+++ pages/requests/Requests.js
@@ -1,20 +1,11 @@
 import classNames from "classnames/bind";
 import styles from "./Requests.module.scss";
-import Select from "../../components/Select";
 import data from "../../utils/testData/testRequestArr";
+import TableRow from "../../components/TableRow";
 
 const cx = classNames.bind(styles);
 
 export default function Requests() {
-  const dataMaker = (date) => {
-    const rawData = new Date(date);
-    const adjustedDate = rawData.toLocaleDateString("en-GB", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-    });
-    return adjustedDate;
-  };
   return (
     <section className={styles.requests}>
       <div className={styles.container}>
@@ -34,26 +25,17 @@ export default function Requests() {
           <tbody className={styles.tableBody}>
             {data.requests.map(
               ({ id, date, status, admin, name, tel, info }) => (
-                <tr
+                <TableRow
                   key={id}
-                  className={cx("tableRow", {
-                    new: status === "новий",
-                  })}
-                >
-                  <td className={cx("tableCell", "number")}>{id}.</td>
-                  <td className={cx("tableCell", "date")}>{dataMaker(date)}</td>
-                  <td className={cx("tableCell", "status")}>
-                    <Select
-                      defaultValue={status}
-                      options={data.statusOptions}
-                      type="table"
-                    />
-                  </td>
-                  <td className={cx("tableCell", "admin")}>{admin || "-"}</td>
-                  <td className={cx("tableCell", "name")}>{name}</td>
-                  <td className={cx("tableCell", "tel")}>{tel}</td>
-                  <td className={cx("tableCell", "info")}>{info}</td>
-                </tr>
+                  id={id}
+                  date={date}
+                  status={status}
+                  admin={admin}
+                  name={name}
+                  tel={tel}
+                  info={info}
+                  options={data.statusOptions}
+                />
               )
             )}
           </tbody>
