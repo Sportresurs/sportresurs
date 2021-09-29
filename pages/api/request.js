e92a2d256e180764fa08d7cf503d9b4b46612857
Andrii <andrijkotsupyr@gmail.com>
2021-09-29 16:00:08 +0300

    T-128 update request route

--- pages/api/request.js
+++ pages/api/request.js
@@ -1,34 +1,20 @@
 import nc from "next-connect";
 import { withSentry } from "@sentry/nextjs";
-import Request from "../../models/request";
+import { Request } from "../../models";
 
-const handler = nc()
-  .post(async (req, res) => {
-    try {
-      const { date, status, admin, name, tel, info } = req.body;
-      const newRequest = await Request.create({
-        date,
-        status,
-        admin,
-        name,
-        tel,
-        info,
-      });
-      res.statusCode = 201;
-      res.json(newRequest);
-    } catch (err) {
-      res.json(err);
-    }
-  })
-  .get(async (req, res) => {
-    await Request.findAll()
-      .then((requests) => {
-        res.statusCode = 200;
-        res.json(requests);
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+const handler = nc().post(async (req, res) => {
+  try {
+    const { name, phone, info } = req.body;
+    const newRequest = await Request.create({
+      name,
+      phone,
+      info,
+    });
+    res.statusCode = 201;
+    res.json(newRequest);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 export default withSentry(handler);
