54a4655442aa4e423effd00b961e68353231f087
Dimirolz <dimirolz@gmail.com>
2021-10-05 14:57:15 +0300

    T-188 Delete news (#108)
    * T-118 Blocked news model && migration
    
    * T-118 Block news api route
    
    * T-118 Filter news && clear old blocks
    
    * T-118 Fix env && fix multiple sql invocation
    
    * T-118 Fix to guard && fix async function

--- utils/instagram.js
+++ utils/instagram.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 import * as Sentry from "@sentry/nextjs";
+import { BlockedNews } from "../models";
 
-const LIMIT_POSTS = 15;
+const LIMIT_POSTS = process.env.LIMIT_INSTAGRAM_POSTS || 30;
 const INSTAGRAM_PAGE_ID = 46497980119;
 const QUERY_HASH = "472f257a40c653c64c666ce877d59d2b";
 const MINUTES_15 = 900000;
+const LIMIT_POSTS_FOR_FRONT =
+  process.env.LIMIT_INSTAGRAM_INSTAGRAM_POSTS_FOR_FRONT || 15;
 
 const PARAMS = [
   `query_hash=${QUERY_HASH}`,
@@ -21,7 +24,30 @@ const lastReq = {
   time: 0,
 };
 
-const getNewsFromInstagram = async () => {
+const getBlockedIds = async () => {
+  const blockedNews = await BlockedNews.findAll({
+    attributes: ["instagram_id"],
+  });
+  return blockedNews.map(({ instagram_id: instagramId }) => instagramId);
+};
+
+const filterNews = async (news, blockedIds) =>
+  news.filter(({ id }) => !blockedIds.includes(id));
+
+const clearOldBlockedNews = (news, blockedIds) => {
+  const newsIds = news.map(({ id }) => id);
+  const oldBlocks = blockedIds.filter((id) => !newsIds.includes(id));
+  if (oldBlocks.length === 0) {
+    return undefined;
+  }
+  return BlockedNews.destroy({
+    where: {
+      instagram_id: oldBlocks,
+    },
+  });
+};
+
+export const getNewsFromInstagram = async () => {
   const timeNow = Date.now();
   if (lastReq.time + MINUTES_15 > timeNow) {
     return lastReq.news;
@@ -46,10 +72,14 @@ const getNewsFromInstagram = async () => {
         };
       });
 
-    lastReq.news = newsFromInstagram;
+    const blockedIds = await getBlockedIds();
+    await clearOldBlockedNews(newsFromInstagram, blockedIds);
+    const filteredNews = (
+      await filterNews(newsFromInstagram, blockedIds)
+    ).slice(0, LIMIT_POSTS_FOR_FRONT);
+    lastReq.news = filteredNews;
     lastReq.time = timeNow;
-
-    return newsFromInstagram;
+    return filteredNews;
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err);
@@ -58,4 +88,7 @@ const getNewsFromInstagram = async () => {
   }
 };
 
-export default getNewsFromInstagram;
+export const clearNewsCache = () => {
+  lastReq.news = [];
+  lastReq.time = 0;
+};
