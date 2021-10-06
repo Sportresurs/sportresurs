7b1efad5df6723b130fa2f394a16bb0a6a25158d
xaarxus <keyshet@gmail.com>
2021-10-06 12:21:22 +0300

    T-134 add update status admin

--- middleware/checkAuthAdmin.js
+++ middleware/checkAuthAdmin.js
@@ -1,11 +1,18 @@
 import { getSession } from "next-auth/client";
+import { User } from "../models";
 
 const checkAuthAdmin = async (req, res, next) => {
   const session = await getSession({ req });
-  if (!session) {
+  const { email } = session.user;
+  const user = await User.findOne({ where: { email } });
+  if (!session || user.status === "deleted") {
     return res.status(401).send("You don't have access.");
   }
-  next();
+  if (user.status !== "confirmed") {
+    user.status = "confirmed";
+    await user.save();
+  }
+  return next();
 };
 
 export default checkAuthAdmin;
