444d186e23005424c433d0fa4127002d94f07313
Sergey Cherkasov <33036302+xaarxus@users.noreply.github.com>
2021-10-06 17:50:49 +0300

    T-134 Add admin email and updated status (#115)
    * T-134 add update status admin
    
    * T-134 add admin logic
    
    * T-134 fix auth and admin logic
    
    * T-134 change import
    
    * T-134 add sendInviteAdmin
    
    * T-134 change link in message

---
+++ pages/api/admin/add-admin.js
@@ -0,0 +1,25 @@
+import { captureException, withSentry } from "@sentry/nextjs";
+import nc from "next-connect";
+import { User } from "../../../models";
+import checkAuthAdmin from "../../../middleware/checkAuthAdmin";
+import { sendInviteAdmin } from "../../../utils/emailSender";
+
+const handler = nc()
+  .use(checkAuthAdmin)
+  .post(async (req, res) => {
+    try {
+      const { email } = req.body;
+      const candidate = await User.findOne({ where: { email } });
+      if (candidate) {
+        return res.status(409).send("This email is already in use!");
+      }
+      await User.create({ email, role: "admin", status: "pending" });
+      sendInviteAdmin(email);
+      return res.status(200).send("Admin added!");
+    } catch (err) {
+      captureException(err);
+      return res.status(500).send({ error: err.name });
+    }
+  });
+
+export default withSentry(handler);
