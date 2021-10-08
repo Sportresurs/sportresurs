0473e0e95e3346206b5e3178b1ebcaa0937cf092
xaarxus <keyshet@gmail.com>
2021-10-08 11:01:34 +0300

    T-122 change context

--- context/context.js
+++ context/context.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const Context = createContext("");
 
@@ -7,15 +8,41 @@ const ContextProvider = ({ children }) => {
   const [filterData, setFilterData] = useState({
     purposeOfAreas: [],
     districts: [],
+    rating: { value: 0 },
   });
+  const [filterFields, setFilterFields] = useState({
+    purposes: [],
+    districts: [],
+  });
+  const [areas, setAreas] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `${process.env.NEXT_PUBLIC_API_URL}/areas/filter-fields`,
+    }).then(({ data }) => setFilterFields(data));
+    axios({
+      method: "GET",
+      url: `${process.env.NEXT_PUBLIC_API_URL}/areas`,
+    }).then(({ data }) => setAreas(data.areas));
+  }, []);
 
   const handleCoordinates = (value) => {
     setCoordinates(value);
   };
-  const handleFilterData = (e) => {
-    setFilterData([
-      { label: e.target.textContent, value: e.target.textContent },
-    ]);
+  const handleFilterDistrict = (e) => {
+    setFilterData({
+      purposeOfAreas: [],
+      districts: [{ label: e.target.textContent, value: e.target.textContent }],
+    });
+  };
+  const handleFilterPurpose = (e) => {
+    setFilterData({
+      districts: [],
+      purposeOfAreas: [
+        { label: e.target.textContent, value: e.target.textContent },
+      ],
+    });
   };
 
   return (
@@ -23,8 +50,12 @@ const ContextProvider = ({ children }) => {
       value={{
         coordinates,
         filterData,
+        filterFields,
+        areas,
+        setAreas,
         handleCoordinates,
-        handleFilterData,
+        handleFilterDistrict,
+        handleFilterPurpose,
       }}
     >
       {children}
