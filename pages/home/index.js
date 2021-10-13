d2b1e0a223aabf26a2eafd60863c601b9400892c
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-10-13 16:31:44 +0300

    t-174-add fetch top courts from DB

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,17 +4,27 @@ import { Grid } from "../../components/Grid";
 import styles from "./Home.module.scss";
 import SearchSection from "../../components/SearchSection";
 import News from "../../components/News";
-import topPlaygrounds from "../../utils/topPlaygrounds";
 
-export default function Home() {
+export default function Home({ topPlaygrounds }) {
   return (
     <div className={styles.background}>
       <Grid>
         <SearchSection />
-        <TopCourts courtList={topPlaygrounds} />
+        {topPlaygrounds && <TopCourts courtList={topPlaygrounds} />}
         <About />
         <News />
       </Grid>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}api/areas`);
+  const data = await res.json();
+  const topPlaygrounds = data.areas.filter((el) => el.featured);
+  return {
+    props: {
+      topPlaygrounds,
+    },
+  };
 }
