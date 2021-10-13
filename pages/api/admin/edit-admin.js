073da38f90c0c92e46a980aa8c0becba47b0cc9f
Andrii <andrijkotsupyr@gmail.com>
2021-10-13 16:21:06 +0300

    T-135 update edit admin route

--- pages/api/admin/edit-admin.js
+++ pages/api/admin/edit-admin.js
@@ -7,12 +7,13 @@ const handler = nc()
   .use(checkAuthAdmin)
   .patch(async (req, res) => {
     try {
-      const { id, email } = req.body;
-      const candidate = await User.findOne({ where: id });
+      const { email } = req.body;
+      const { id } = req.query;
+      const candidate = await User.findOne({ where: { id } });
       if (!candidate) {
         return res.status(409).send("This email does not exist!");
       }
-      await User.update({ email }, { where: id });
+      await User.update({ email }, { where: { id } });
       return res.status(200).send("Admin successfully updated!");
     } catch (err) {
       captureException(err);
