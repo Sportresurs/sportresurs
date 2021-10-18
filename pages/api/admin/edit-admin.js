992d1daacab05413f27da28034cab70e23fac8ad
Andrii Kotsupyr <66208186+Andrii-25@users.noreply.github.com>
2021-10-18 09:46:34 +0300

    T-135 Backend for page admins (#129)
    * T-135 add get admins route
    
    * T-135 add update admin route
    
    * T-135 add delete admin route
    
    * T-135 update edit admin route
    
    * T-135 update get admins route
    
    * T-135 update edit admin route
    
    * T-135 update delete admin route
    
    Co-authored-by: Andrii <andrijkotsupyr@gmail.com>

---
+++ pages/api/admin/edit-admin.js
@@ -0,0 +1,24 @@
+import { captureException, withSentry } from "@sentry/nextjs";
+import nc from "next-connect";
+import { User } from "../../../models";
+import checkAuthAdmin from "../../../middleware/checkAuthAdmin";
+
+const handler = nc()
+  .use(checkAuthAdmin)
+  .patch(async (req, res) => {
+    try {
+      const { email } = req.body;
+      const { id } = req.query;
+      const candidate = await User.findOne({ where: { id } });
+      if (!candidate) {
+        return res.status(409).send("This email does not exist!");
+      }
+      await User.update({ email }, { where: { id } });
+      return res.status(200).send("Admin successfully updated!");
+    } catch (err) {
+      captureException(err);
+      return res.status(500).send({ error: err.name });
+    }
+  });
+
+export default withSentry(handler);
