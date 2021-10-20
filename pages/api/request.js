bb136f806e7d09b02d919e5d8faac8b0c631d643
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-10-20 10:59:10 +0300

    t-120-add get/post method for requests

--- pages/api/request.js
+++ pages/api/request.js
@@ -3,20 +3,40 @@ import { withSentry, captureException } from "@sentry/nextjs";
 import { Request } from "../../models";
 import { sendRequest } from "../../utils/emailSender";
 
-const handler = nc().post(async (req, res) => {
-  try {
-    const { name, phone, info } = req.body;
-    const newRequest = await Request.create({
-      name,
-      phone,
-      info,
-    });
-    sendRequest(name, phone, info);
-    res.status(201).json(newRequest);
-  } catch (err) {
-    res.json(err);
-    captureException(err);
-  }
-});
+const handler = nc()
+  .get(async (req, res) => {
+    const userRequests = await Request.findAll();
+    return res.status(200).json({ userRequests });
+  })
+  .post(async (req, res) => {
+    try {
+      const { name, phone, info } = req.body;
+      const newRequest = await Request.create({
+        name,
+        phone,
+        info,
+      });
+      sendRequest(name, phone, info);
+      res.status(201).json(newRequest);
+    } catch (err) {
+      res.json(err);
+      captureException(err);
+    }
+  })
+  .patch(async (req, res) => {
+    const { id, status, email } = req.body;
+    const request = await Request.findOne({ where: { id } });
+    if (!request) {
+      return res
+        .status(200)
+        .send({ error: { message: "request ID doesn't exsist" } });
+    }
+    request.status = status;
+    request.admin_email = email;
+
+    await request.save();
+
+    return res.status(200).json({ request });
+  });
 
 export default withSentry(handler);
