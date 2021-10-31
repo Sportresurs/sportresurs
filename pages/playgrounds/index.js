f8febd4947d80f12393f81e90690fac652bbc8e4
Andrii <kotsupyr.a@gmail.com>
2021-10-31 22:35:12 +0200

    T-132 add plus icon to playgrounds page

--- pages/playgrounds/index.js
+++ pages/playgrounds/index.js
@@ -1,18 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useSession } from "next-auth/client";
 import { Context } from "../../context";
 import s from "./Playgrounds.module.scss";
 import CourtCard from "../../components/CourtCard";
 import { Grid } from "../../components/Grid";
 import Filters from "../../components/Filters";
+import PlusIcon from "../../components/PlusIcon/PlusIcon";
+import AdminPlaygroundModal from "../../components/AdminPlaygroundModal";
+import useModalHandlers from "../../utils/hooks/useModalHandlers";
 
 export default function Playgrounds({ playgrounds }) {
   const { areas } = useContext(Context);
+  const [isModalShown, handleOpenModal, handleCloseModal] = useModalHandlers();
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [session] = useSession();
+
+  useEffect(() => {
+    if (session) {
+      setIsAdmin(true);
+    }
+  }, [isAdmin, session]);
 
   return (
     <div className={s.background}>
       <section className={s.courts}>
         <Grid>
           <Filters areas={playgrounds} />
+          {isAdmin ? <PlusIcon onClick={() => handleOpenModal()} /> : null}
           <ul className={s.list}>
             {areas.map((court) => (
               <li key={court.id} className={s.listItem}>
@@ -22,6 +36,7 @@ export default function Playgrounds({ playgrounds }) {
           </ul>
         </Grid>
       </section>
+      <AdminPlaygroundModal visible={isModalShown} onClose={handleCloseModal} />
     </div>
   );
 }
