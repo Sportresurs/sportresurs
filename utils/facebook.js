bb188d5fc213d6f728b394175e80e5c26888e9c5
TarasPyzo <tyro.asap@gmail.com>
2022-08-10 12:30:36 +0300

    T-231: update facebook service logic & tokens

--- utils/facebook.js
+++ utils/facebook.js
@@ -2,17 +2,15 @@ import axios from "axios";
 import * as Sentry from "@sentry/nextjs";
 import { BlockedNews } from "../models";
 
-const {
-  FB_CLIENT_ID,
-  FB_CLIENT_SECRET,
-  FB_ACCESS_TOKEN,
-  LIMIT_FB_POSTS,
-  LIMIT_FB_POSTS_FOR_FRONT,
-} = process.env;
+const FB_LONG_LIVED_ACCESS_TOKEN = process.env.FB_LONG_LIVED_ACCESS_TOKEN;
+const FB_LKPSPORTRESURS_USER_ID = process.env.FB_LKPSPORTRESURS_USER_ID;
+const FB_API_VERSION = process.env.FB_API_VERSION;
+const LIMIT_FB_POSTS = process.env.LIMIT_FB_POSTS;
+const LIMIT_FB_POSTS_FOR_FRONT = process.env.LIMIT_FB_POSTS_FOR_FRONT;
 
 const MINUTES_15 = 900000;
 const LAST_REQUEST = { news: [], time: 0 };
-const LONG_TOKEN_FB = { value: null, month: null };
+// const LONG_TOKEN_FB = { value: null, month: null };
 
 const getBlockedIds = async () => {
   const blockedNews = await BlockedNews.findAll({
@@ -37,6 +35,7 @@ const clearOldBlockedNews = (news, blockedIds) => {
   });
 };
 
+/*
 const getLongLivedToken = async () => {
   const monthNow = new Date().getMonth();
   if (LONG_TOKEN_FB.month === monthNow) {
@@ -47,8 +46,8 @@ const getLongLivedToken = async () => {
     {
       params: {
         grant_type: "fb_exchange_token",
-        client_id: FB_CLIENT_ID,
-        client_secret: FB_CLIENT_SECRET,
+        client_id: process.env.FB_CLIENT_ID,
+        client_secret: process.env.FB_CLIENT_SECRET,
         fb_exchange_token: FB_ACCESS_TOKEN,
       },
     }
@@ -56,23 +55,27 @@ const getLongLivedToken = async () => {
   LONG_TOKEN_FB.month = monthNow;
   LONG_TOKEN_FB.value = data.access_token;
 };
+*/
 
 const getPosts = async () => {
-  const timeNow = Date.now();
-  if (LAST_REQUEST.time + MINUTES_15 > timeNow) {
+  const now = Date.now();
+  if (
+    LAST_REQUEST.news.length
+    && LAST_REQUEST.time
+    && (LAST_REQUEST.time + MINUTES_15 > now)) {
     return LAST_REQUEST.news;
   }
   try {
-    await getLongLivedToken();
+    // await getLongLivedToken();
     const {
       data: { data },
     } = await axios.get(
-      "https://graph.facebook.com/v12.0/lkpsportresurs/posts",
+      `https://graph.facebook.com/${FB_API_VERSION}/${FB_LKPSPORTRESURS_USER_ID}/posts`,
       {
         params: {
           fields: "message,id,full_picture,permalink_url",
           limit: LIMIT_FB_POSTS,
-          access_token: LONG_TOKEN_FB.value,
+          access_token: FB_LONG_LIVED_ACCESS_TOKEN,
         },
       }
     );
@@ -92,7 +95,8 @@ const getPosts = async () => {
       LIMIT_FB_POSTS_FOR_FRONT
     );
     LAST_REQUEST.news = filteredNews;
-    LAST_REQUEST.time = timeNow;
+    LAST_REQUEST.time = now;
+
     return filteredNews;
   } catch (err) {
     // eslint-disable-next-line no-console
